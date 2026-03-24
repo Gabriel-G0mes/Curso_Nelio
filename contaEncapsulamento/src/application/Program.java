4067dfac83113bf94d9418ee4d08cf18d4e091d5
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-03-24 05:57:55 -0300

    Encapsulamento exer

---
+++ contaEncapsulamento/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Conta;
+
+public class Program {
+    public void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+
+
+        input.close();
+    }
+}
---
+++ contaEncapsulamento/src/entities/Conta.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Conta {
+    private String nome;
+    private double saldo;
+
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
